26d1e912fe5ea73e2b7da6d272f2450bc5c2c83c
PavelHavelka <stromoclimber@gmail.com>
2023-04-11 21:13:31 +0200

    novy commit

--- Pokemon API/Controllers/HomeController.cs
+++ Pokemon API/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Pokemon_API.Models;
 using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace Pokemon_API.Controllers
 {
@@ -15,13 +19,24 @@ namespace Pokemon_API.Controllers
 
         public IActionResult Index()
         {
+            //vytvori request na api
+            WebRequest request = WebRequest.Create("http://pokeapi.co/api/v2/pokemon/1");
+            //posle request pryc
+            WebResponse response = request.GetResponse();
+            //get back to response stream
+            Stream stream = response.GetResponseStream();
+            //make it accessible
+            StreamReader reader = new StreamReader(stream);
+            //vezme vse ze streamReadru a da to do stringu jako json format
+            string responseFromServer = reader.ReadToEnd();
+            JObject parsedString = JObject.Parse(responseFromServer);
+            Pokemon myPokemon = parsedString.ToObject<Pokemon>();
+            Debug.WriteLine(myPokemon.moves[0].move.name);
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
